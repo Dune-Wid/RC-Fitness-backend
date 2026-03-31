dcc3d3002e1fa3993519aa99898e68fd30ef2930
O S K SENADHEERA <sathsarabinary2004@gmail.com>
2026-03-31 13:29:32 +0530

    added invoice emailing function

--- models/Payment.js
+++ models/Payment.js
@@ -5,6 +5,7 @@ const paymentSchema = new mongoose.Schema({
   date: { type: String, required: true },
   status: { type: String, required: true },
   duration: { type: String },
+  email: { type: String },
   createdAt: { type: Date, default: Date.now }
 });
 module.exports = mongoose.model('Payment', paymentSchema);
